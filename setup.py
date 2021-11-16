42148603d3bc0fb3cc19011cb0ce85e122b6f30f
lab-charles <haung7tjsl@gmail.com>
2021-11-16 20:00:16 +0800

    update version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="p-stable-lsh-python",
-    version="0.0.1",
+    version="0.0.2",
     author="Charles7",
     author_email="liuhuiqi@mail.ustc.edu.cn",
     description="A 1&2-stable Locality-Sensitive Hashing implementation in Python",
